131105150cd18873cfce1288f65ed1daa6087444
Ankit_Choudhary <ankitsocial16@gmail.com>
2019-05-22 17:10:43 +0530

    alias, css support

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'
 // import { startClock, serverRenderClock } from '../src/store'
-import Template from '../src/components/templates/login';
-
-import { login } from "../src/reduxHelper"
+import "./main.css"
 import Router from 'next/router'
+
+
 class App extends React.Component {
+
   constructor(props){
     super(props)
   }
@@ -27,39 +28,12 @@ class App extends React.Component {
     // })
   }
 
-  handleFormData(data) {
-    // TODO: check later , wher id is coming from in formstate
-    let formData = data.values
-    this.props.loginDispatch(formData).then(res=>{
-      console.log("response", res)
-    // login was successful
-      // check if account is blocked
-      if(res.data.isblocked){
-        alert("You account is not active")
-      }else {
-        if(!res.data.data1){
-          Router.push("/business-details")
-        }else if (!res.data.data2){
-          Router.push("/master-data")
-        }else{
-          Router.push("/dashboard")
-        }
-      }  
-    }).catch(err =>{     
-      console.log(err)
-      if(err.reponse && (err.response.status = 401)){
-        alert("Login Failed")
-      }else {
-        alert("Something bad happend at our end")
-      }
-      console.log("error", err)
-    })
-  }
+ 
 
  render() {
     return (
       <div> 
-        <Template getFormData={(data) => this.handleFormData(data)}/>
+        <button>Test button</button>
       </div>
     );
   }
@@ -67,10 +41,10 @@ class App extends React.Component {
 
 
 const mapStateToProps = state => ({
-  login: state.login
+  
 })
 // Example Syntax for writing dispatch
 const mapDispatchToProps = dispatch => ({
-  loginDispatch: (username, password) => dispatch(login.action(username, password)),
+  
 })
 export default connect(mapStateToProps,mapDispatchToProps)(App);
